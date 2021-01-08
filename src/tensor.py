3c4f9ebae0cc5e247a34b8ec4b8b93a75a94b633
jonveroro <jonveroro04@gmail.com>
2021-01-09 01:45:52 +0800

    add tensor mean ops

--- src/tensor.py
+++ src/tensor.py
@@ -1,6 +1,6 @@
 '''
     Description:
-        Contains all tensor related class
+        Contains all tensor related class. Performs autograd functionality for every tensors
 
     References:
         https://github.com/geohot/tinygrad/blob/master/tinygrad/ops_cpu.py
@@ -8,7 +8,7 @@
 
 '''
 
-# TODO: Create grad tracking per tensor object
+# TODO: Add tensor movement ops
 import time
 import numpy as np
 from copy import deepcopy,copy
@@ -21,6 +21,7 @@ class Ops:
     def __init__(self):
         self._backward = lambda: None
 
+    # * basic ops
 
     def sub(self,weight):
         out =Tensor(self.val-weight.val,(self,weight),'sub')
@@ -29,6 +30,7 @@ class Ops:
         def __backward():
             self.grad = self.unbroadcast(out.grad,self.val.shape)
             weight.grad = self.unbroadcast(-out.grad,self.weight.shape)
+            assert(self.grad.shape == self.val.shape)
 
         out._backward = __backward
         return out
@@ -43,6 +45,7 @@ class Ops:
         def __backward():
             self.grad = out.grad.dot(self.weight.val.T)
             weight.grad = self.val.T.dot(out.grad)
+            assert(self.grad.shape == self.val.shape)
         
         out._backward = __backward
 
@@ -71,6 +74,7 @@ class Ops:
         def __backward():
             self.grad = self.unbroadcast(out.grad,self.val.shape)
             weight.grad = self.unbroadcast(out.grad,self.weight.shape)
+            assert(self.grad.shape == self.val.shape)
 
         out._backward = __backward
         return out
@@ -87,10 +91,29 @@ class Ops:
             axis = [self.axis] if type(self.axis) is int else self.axis
             shape = [1 if axis is None or i in axis else out.shape[i] for i in range(len(out.shape))]
             self.grad = out.grad.reshape(shape) + np.zeros_like(self.val)
+            assert(self.grad.shape == self.val.shape)
             
         out._backward = __backward
         return out
 
+    def mean(self,axis=None):
+        #val = self.val.sum(axis=axis)
+        val = self.val.mean()
+        out = Tensor(np.array([val]),(self,deepcopy(self)),'mean')
+        
+        # * backward function
+        def __backward():
+            axis = [self.axis] if type(self.axis) is int else self.axis
+            shape = [1 if axis is None or i in axis else out.shape[i] for i in range(len(out.shape))]
+            self.grad = (out.grad.reshape(shape) + np.zeros_like(self.val)) * ((np.prod(out.shape)/np.prod(self.shape)))
+            assert(self.grad.shape == self.val.shape)
+            
+        out._backward = __backward
+        return out
+        
+
+
+    # * activations
     def relu(self):
         out = Tensor(activations().relu(self.val),(self,deepcopy(self)),'relu')
         # * backward
@@ -102,12 +125,11 @@ class Ops:
 
     
     def sigmoid(self):
-        # e = np.exp(self.val)
-        # val = e * ((1 + e) ** -1.0)
         out = Tensor(activations().sigmoid(self.val),(self,deepcopy(self)),'sigmoid')
         # * backward
         def __backward():
             self.grad = activations().d_sigmoid(out.val)
+            assert(self.grad.shape == self.val.shape)
 
         out._backward = __backward
         return out
@@ -117,12 +139,12 @@ class Ops:
         # * backward
         def __backward():
             self.grad = activations().d_tanh(out.val)
-            print(self.grad)
+            assert(self.grad.shape == self.val.shape)
 
         out._backward = __backward
         return out
 
-
+    # * etc
     def unbroadcast(self,out, in_sh):
         # adjoint operation to broadcast is sum. Need to sum all axis with 1 = in_sh[i] < out.shape[i]
         sum_axis = tuple([i for i in range(len(in_sh)) if in_sh[i]==1 and out.shape[i]>1]) if in_sh != (1,) else None
@@ -133,7 +155,7 @@ class Tensor(Ops):
     '''
         Wraps additional functions to numpy object
     '''
-    def __init__(self,val=None,_cache=(), _op='init',**kwargs):
+    def __init__(self,val=None,_cache=(), _op='init',requires_grad=True,**kwargs):
         super().__init__()
         if val is not None:
             self.val = np.array(val,dtype=np.float32,**kwargs)
@@ -147,6 +169,7 @@ class Tensor(Ops):
         self.ops = _op 
         self._sequence = []
         self.axis = None
+        self.requires_grad = requires_grad
 
     def __repr__(self):
         return f"Tensor({self.val})"
@@ -192,19 +215,6 @@ class Tensor(Ops):
         return self
 
 
-    def _get_sequence(self):
-        sequence = []
-        visited = set()
-
-        def build_seq(v):
-            if v not in visited:
-                visited.add(v)
-                for child in v._prev:
-                    build_seq(child)
-                sequence.append(v)
-
-        return sequence
-
     def backward(self):
         assert(self.shape == (1,))
         # * get tensor traversal sequence
@@ -223,13 +233,14 @@ class Tensor(Ops):
         __get_traversal(self)
         # * apply backward steps for tensors
         self._sequence = sequence
-        # print(len(sequence))
         # * create default grad val in shape of current value
         self.grad =  np.ones(self.val.shape,dtype=self.val.dtype)
         # * backward prop loop
         for t in reversed(sequence):
             if t.ops != 'init':
-                t._backward()
+                if t.requires_grad == True:
+                    assert (t.grad is not None)
+                    t._backward()
     
 
     def history(self):
@@ -239,18 +250,14 @@ class Tensor(Ops):
             
 
 
-
-
 if __name__ == "__main__":
 
     start_time = time.time()
     x = Tensor().eye((3,3))
     y = Tensor([[2.0,0,-2.0]])
     b = Tensor().eye((1,1))
-    # x = Tensor().random((10,10))
-    # y = Tensor().random((1,10))
-    # b = Tensor().random((1,1))
-    z = y.dot(x).add(b).sum()
+
+    z = y.dot(x).add(b).mean()
     print('z',z)
     z.backward()
     print("--- %s seconds ---" % (time.time() - start_time))
