a727c76e2a7cd8702c202e537956cd48623f707a
jonveroro <jonveroro04@gmail.com>
2021-01-08 12:02:17 +0800

    fix autograd and grad tracking per tensor variable

--- src/tensor.py
+++ src/tensor.py
@@ -1,61 +1,139 @@
 '''
     Description:
         Contains all tensor related class
-    Author:
-        Jonver Oro
+
+    References:
+        https://github.com/geohot/tinygrad/blob/master/tinygrad/ops_cpu.py
+        https://github.com/karpathy/micrograd/blob/master/micrograd/engine.py
 
 '''
 
 # TODO: Create grad tracking per tensor object
 
 import numpy as np
-from copy import deepcopy
+from copy import deepcopy,copy
 from activations import activations
 
-class TensorTrack:
+class Ops:
     '''
-        Serves as cache for tensors
+        Contains all tensor operations
     '''
     def __init__(self):
-        self.tensors = []
+        self._backward = lambda: None
 
-    def __repr__(self):
-        return f"Total {len(self.tensors)} tensors states.\n {self.tensors} "
+    def dot(self,weight):
 
-    def save(self,tensor):
-        #self.tensors = tensor.cache.tensors
-        self.tensors.append(tensor)
+        out = Tensor(self.val.dot(weight.val), (self, weight), 'dot')
+        self.weight = weight
 
-    def combine(self,cache):
-        self.tensors = cache.tensors + self.tensors
+        # * backward function
+        def __backward():
+            self.grad = out.grad.dot(self.weight.val.T)
+            weight.grad = self.val.T.dot(out.grad)
+        
+        out._backward = __backward
+
+        return out
+
+
+    def mul(self, weight):
+        
+        out = Tensor(self.val * weight.val, (self, weight), 'mul')
+        self.weight = weight
+
+        # * backward function
+        def __backward():
+            self.grad = weight.val * out.grad
+            weight.grad = self.val * out.grad
+
+        out._backward = __backward
+        return out
+
+    def add(self,weight):
+
+        out = Tensor(self.val + weight.val, (self,weight),'add')
+        self.weight = weight
+
+        # * backward function
+        def __backward():
+            self.grad = self.unbroadcast(out.grad,self.val.shape)
+            weight.grad = self.unbroadcast(out.grad,self.weight.shape)
+
+        out._backward = __backward
+        return out
 
     
 
-class Tensor:
+    def sum(self,axis=None):
+        val=  np.array([self.val.sum()]) if axis is None else self.val.sum(axis=axis)
+        out = Tensor(val,(self,deepcopy(self)),'sum')
+        self.axis = axis
+
+        # * backward function
+        def __backward():
+            axis = [self.axis] if type(self.axis) is int else self.axis
+            shape = [1 if axis is None or i in axis else out.shape[i] for i in range(len(out.shape))]
+            self.grad = out.grad.reshape(shape) + np.zeros_like(self.val)
+            
+        out._backward = __backward
+        return out
+
+    def relu(self):
+        out = Tensor(activations().relu(self.val),(self,deepcopy(self)),'relu')
+        # * backward
+        def __backward():
+            self.grad = activations().d_relu(out.val)
+
+        out._backward = __backward
+        return out
+
+    
+    def sigmoid(self):
+        # e = np.exp(self.val)
+        # val = e * ((1 + e) ** -1.0)
+        out = Tensor(activations().sigmoid(self.val),(self,deepcopy(self)),'sigmoid')
+        # * backward
+        def __backward():
+            self.grad = activations().d_sigmoid(out.val)
+
+        out._backward = __backward
+        return out
+
+    def tanh(self):
+        out = Tensor(activations().tanh(self.val),(self,deepcopy(self)),'tanh')
+        # * backward
+        def __backward():
+            self.grad = activations().d_tanh(out.val)
+            print(self.grad)
+
+        out._backward = __backward
+        return out
+
+
+    def unbroadcast(self,out, in_sh):
+        # adjoint operation to broadcast is sum. Need to sum all axis with 1 = in_sh[i] < out.shape[i]
+        sum_axis = tuple([i for i in range(len(in_sh)) if in_sh[i]==1 and out.shape[i]>1]) if in_sh != (1,) else None
+        return out.sum(axis=sum_axis).reshape(in_sh)
+        
+
+class Tensor(Ops):
     '''
         Wraps additional functions to numpy object
     '''
-    def __init__(self,val=None,transpose=False,inherit=True,requires_grad=True,**kwargs):
-        # self.cache = [] # * stores previous values for derivative computations
-        self.cache = TensorTrack()
+    def __init__(self,val=None,_cache=(), _op='init',**kwargs):
+        super().__init__()
         if val is not None:
-            self.val = np.array(val,**kwargs)
+            self.val = np.array(val,dtype=np.float32,**kwargs)
         else:
             self.val = None
-        if transpose ==  True:
-            self.val = self.val.T
 
-        # * default variables
-        self.ops='assign'
-        self.grad = None
-        self.act = activations()
-        self.inherit = inherit
-        self.requires_grad = requires_grad
+        self.grad = 0
         self.weight = None
-        self.weight_grad = None
-
-        # * save initial copy
-        # self.cache.save(self)
+        
+        self._prev = set(_cache)
+        self.ops = _op 
+        self._sequence = []
+        self.axis = None
 
     def __repr__(self):
         return f"Tensor({self.val})"
@@ -68,7 +146,7 @@ class Tensor:
     def dtype(self):
         return self.val.dtype
 
-    
+
     # * tensor default value generation function
     def eye(self,shape,**kwargs):
         assert(shape[0] >0)
@@ -101,237 +179,66 @@ class Tensor:
         return self
 
 
-    # * TENSOR MAIN OPERATIONS
+    def _get_sequence(self):
+        sequence = []
+        visited = set()
 
-    def unbroadcast(self,out, in_sh):
-        # adjoint operation to broadcast is sum. Need to sum all axis with 1 = in_sh[i] < out.shape[i]
-        sum_axis = tuple([i for i in range(len(in_sh)) if in_sh[i]==1 and out.shape[i]>1]) if in_sh != (1,) else None
-        return out.sum(axis=sum_axis).reshape(in_sh)
+        def build_seq(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_seq(child)
+                sequence.append(v)
 
-    def mul(self,tensor):
-        ops_val = 'mul'
-        self.cache.combine(tensor.cache)
-        self.weight = tensor.val
-        val = self.val * tensor.val
-        return self.__compile(val,self.inherit,ops_val,weight=tensor.val)
-
-    def mul_grad(self,tensor):
-        val_shape = self.val.shape
-        weight_shape = self.weight.shape
-        dx = self.unbroadcast(self.weight*tensor.val,val_shape)
-        dy = self.unbroadcast(self.val*tensor.val,weight_shape)
-        return dy,dx
-
-    # * mean
-    def mean(self):
-        ops_val = 'mean'
-        val = np.array([np.mean(self.val)])
-        return self.__compile(val,self.inherit,ops_val)
-
-    # * add
-    def add(self,tensor,inherit=True):
-        '''
-            tensor <class>: this is the tensor object to perform operation
-            inherit <bool>: set as true if you want to reassign new value to current tensor
-        '''
-        ops_val = 'add'
-        # self.cache.save(self)
-        self.cache.combine(tensor.cache)
-        self.weight = tensor.val
-        val = self.val + tensor.val
-        #self.grad = tensor.val-self.val
-        return self.__compile(val,inherit,ops_val,weight=tensor.val)
-
-    def add_grad(self,tensor):
-        val_shape = self.val.shape
-        weight_shape = self.weight.shape
-        dx = self.unbroadcast(tensor.val,val_shape)
-        dy = self.unbroadcast(tensor.val,weight_shape)
-        return dy,dx
-    
-
-    # * dot
-    def dot(self,tensor,inherit=True):
-        '''
-            tensor <class>: this is the tensor object to perform operation
-            inherit <bool>: set as true if you want to reassign new value to current tensor
-        '''
-        ops_val = 'dot'
-        # self.cache.save(self)
-        self.cache.combine(tensor.cache)
-        self.weight = tensor.val
-        val = self.val.dot(tensor.val)
-        return self.__compile(val,inherit,ops_val,weight=tensor.val)
-
-    def dot_grad(self,tensor):
-        
-        #print(self.val)
-        dx = tensor.val.dot(self.weight.T)
-        dw = self.val.T.dot(tensor.val)
-        
-        return dw,dx
-
-
-    # * sub
-    def sub(self,tensor,inherit=True):
-        '''
-            tensor <class>: this is the tensor object to perform operation
-            inherit <bool>: set as true if you want to reassign new value to current tensor
-        '''
-        ops_val = 'sub'
-        self.cache.save(self)
-        self.cache.combine(tensor.cache)
-        val = self.val - tensor.val
-        #self.grad = tensor.val-self.val
-        return self.__compile(val,inherit,ops_val)
-
-
-    # * sum
-    def sum(self,axis=None):
-        ops_val = 'sum'
-        #self.cache.save(self)
-        # print(self.cache)
-        val = np.array([self.val.sum()]) if axis is None else self.val.sum(axis=axis)
-        self.weight = axis
-        return self.__compile(val,self.inherit,ops_val)
-
-    def sum_grad(self,tensor):
-
-        axis = self.weight
-        shape = [1 if axis is None or i in axis else self.val.shape[i] for i in range(len(self.val.shape))]
-        return tensor.val.reshape(shape) + np.zeros_like(self.val)
-
-
-    #  * activation operations
-    def sigmoid(self):
-        ops_val = 'sigmoid'
-        val = self.act.sigmoid(self.val)
-        return self.__compile(val,self.inherit,ops_val)
-
-    def sigmoid_grad(self):
-        return self.act.d_sigmoid(self.val)
-
-    def relu(self):
-        ops_val = 'relu'
-        val = self.act.relu(self.val)
-        return self.__compile(val,self.inherit,ops_val)
-    
-    def relu_grad(self):
-        return self.act.d_relu(self.val)
-
-    def tanh(self):
-        ops_val = 'tanh'
-        val = self.act.tanh(self.val)
-        return self.__compile(val,self.inherit,ops_val)
-
-    def tanh_grad(self):
-        return self.act.d_tanh(self.val)
-
-    def transpose(self):
-        self.val = self.val.T
-        return self
-
+        return sequence
 
     def backward(self):
-        #TODO: Implement back-propagation 
-        prev_grad = None
-        cache_copy = []
-        for i,t in enumerate(reversed(self.cache.tensors)):
-            if i == 0 or prev_grad.shape == (1,):
-                # set prev grad as 1 when last tensor in accordance to shape of prev tensor
-                # or when prev grad shape is equals (1,)
-                prev_grad = Tensor(np.ones(self.cache.tensors[i-1].shape),requires_grad=False,inherit=False)
-            else:
-                prev_grad = Tensor(prev_grad,requires_grad=False,inherit=False)
 
-            # * matix ops
-            if t.ops == 'sum':
-                t.grad = t.sum_grad(prev_grad)
-            
-            elif t.ops == 'dot':
-                t.weight_grad,t.grad = t.dot_grad(prev_grad)
+        assert(self.val.shape == (1,))
 
-            elif t.ops == 'add':
-                t.weight_grad,t.grad = t.add_grad(prev_grad)
+        # * get tensor traversal sequence
+        sequence = []
+        traversed = []
 
-            elif t.ops == 'mul':
-                t.weight,t.grad = t.mul_grad(prev_grad)
+        def __get_traversal(tensor):
+            if tensor not in traversed:
+                traversed.append(tensor)
+                for c in tensor._prev:
+                    # * process child tensors
+                    __get_traversal(c)
+                # * append current tensor
+                sequence.append(tensor)
+        # * process itself
+        __get_traversal(self)
+        # * apply backward steps for tensors
+        self._sequence = sequence
 
-            # * activation ops
-            elif t.ops == 'relu':
-                t.grad = t.relu_grad()
-                
-            elif t.ops == 'sigmoid':
-                t.grad = t.sigmoid_grad()
-
-            elif t.ops == 'tanh':
-                t.grad = t.tanh_grad()
-
-            else:
-                t.grad = Tensor(np.ones(self.cache.tensors[i-1].shape),requires_grad=False,inherit=False)
-
-            prev_grad = t.grad
-            cache_copy.append(t)
-
-        self.cache.tensors = cache_copy
-
-
-        return self
-
-    # * others
-    def __compile(self,val,inherit,ops,weight=None):
-        # * compiles return value of tensor
-        if self.inherit ==False:
-            return Tensor(val)
-        else:
-            # * copy current tensor to a new tensor object
-            copy_tensor = Tensor(val)
-            copy_tensor.ops = ops
-            copy_tensor.weight = self.weight
-            self.cache.save(copy_tensor) # * save a copy of itself
-            copy_tensor.cache = self.cache
-
-            # * replace old values
-            self.val = val
-            self.ops = ops
-            
-            return self
-
-    def get_prev(self,step=1):
-        # return previous state of tensor
-        try:
-            assert(len(self.cache.tensors)>0)
-            return self.cache.tensors[len(self.cache.tensors)-step]
-        except AssertionError as e:
-            print('Error: Tensor has no previous states')
-            raise(e)
+        # * create default grad val in shape of current value
+        self.grad =  np.ones(self.val.shape,dtype=self.val.dtype)
+        # * backward prop loop
+        for t in reversed(sequence):
+            t._backward()
+    
 
     def history(self):
-        # * return tensor state changes
-        if len(self.cache.tensors) > 0:
-            print('-- Previous States ---')
-            for c in self.cache.tensors[:len(self.cache.tensors)]:
-                print(f'id: {c._id}',c.shape,c.ops)
-        print('--- Current State ---')
-        print(self.val.shape,self.ops)
-        
+        print('----Tensor state history---')
+        for t in reversed(self._sequence):
+            print(t.shape,t.ops)
 
 
 
 
 if __name__ == "__main__":
-
-    # * test
     x = Tensor().eye((3,3))
     y = Tensor([[2.0,0,-2.0]])
-    z = y.dot(x).sum()
-    z.backward() # * backprop
-    
-    # print derivatives
-    print('derivative values')
-    for c in z.cache.tensors:
-        print(f'--{c.ops}---')
-        print(c.grad) # dz/dy 
-        print(c.weight_grad) # dz/dx
+    b = Tensor().eye((1,1))
+    z = y.dot(x).add(b).sum().sigmoid()
+    print('z',z)
+    z.backward()
+    print('dz',z.grad)
+    print('db',b.grad)
+    print('dy',y.grad)
+    print('dx',x.grad)
+
     
 
